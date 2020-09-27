714bc854893867c983812ed9c6aa4a7faf009882
Martin Peck <martinjohnpeck@gmail.com>
2020-09-27 14:17:03 +0000

    updates to rectangle example

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -16,12 +16,47 @@ fn main() {
         "The area of the rectangle is {}",
         area2(&rect1)
     );
-
+    
+    println!(
+        "The area of the rectangle is {}",
+        rect1.area()
+    );
+    
+    println!("rect1 is {:#?}", rect1);
+    
+    let rect_a = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    
+    let rect_b = Rectangle {
+        width: 10, 
+        height: 40,
+    };
+    
+    let rect_c = Rectangle {
+        width: 60,
+        height: 45,
+    };
+    
+    println!("Can rect_a hold rect_b? {}", rect_a.can_hold(&rect_b));
+    println!("Can rect_a hold rect_c? {}", rect_a.can_hold(&rect_c));
 }
 
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.height * self.width
+    }
+    
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width >= other.width && self.height >= other.height
+    }
 }
 
 fn area1(width: u32, height: u32) -> u32 {
