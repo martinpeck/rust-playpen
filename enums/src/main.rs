e32ce697bbd6e73647c40c95e746bb71b0b33de8
Martin Peck <martinjohnpeck@gmail.com>
2020-09-27 15:01:21 +0000

    playing with match

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -10,4 +10,40 @@ fn main() {
  
     println!("home: {:#?}", home);
     println!("loopback: {:#?}", loopback);
+    
+    let my_coin = Coin::Dime;
+    let my_other_coin = Coin::Quarter(UsState::Washington);
+    
+    let value = value_in_cents(my_coin);
+    let value = value_in_cents(my_other_coin);
+    
+}
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    Washington,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State from {:?}", state);
+            25
+        },
+    }
 }
