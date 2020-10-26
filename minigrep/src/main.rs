16097cc763737305176a1b48665f68717c23d122
Martin Peck <martinjohnpeck@gmail.com>
2020-10-26 11:41:20 +0000

    refactoring

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,16 +1,45 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     println!("{:?}", args);
 
-    let query = &args[1];
-    let filename = &args[2];
-    println!("Searching for '{}'", query);
-    println!("In file '{}'", filename);
+    let config = Config::new(&args).unwrap_or_else( |err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
     
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    println!("Searching for '{}'", config.query);
+    println!("In file '{}'", config.filename);
+    
+    run(config);
+}
+
+fn run(config: Config) {
+    let contents = fs::read_to_string(config.filename)
+        .expect("Something went wrong reading the file");
+
     println!("With text \n\n{}", contents)
+}
+
+
+struct Config {
+    query: String,
+    filename: String}
+    
+impl Config {
+
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        
+        if args.len() < 3 {
+            return Err("not enough arguments")
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        
+        Ok(Config { query, filename })
+    }
 }
